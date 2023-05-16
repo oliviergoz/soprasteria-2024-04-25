1efdb6cfe39e33947b529e9fcfd91abfae4010b0
olivier <o.gozlan@ajc-ingenierie.fr>
2023-05-16 10:27:00 +0200

    creation projet spring mvc pour restcontroller

---
+++ SPRING-MVC/eshopWebService/src/main/java/eshop/configurations/WebConfig.java
@@ -0,0 +1,12 @@
+package eshop.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("eshop.restcontrollers")
+public class WebConfig {
+
+}
