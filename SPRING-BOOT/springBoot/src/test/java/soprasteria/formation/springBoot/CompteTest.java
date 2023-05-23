9be096b0fe99f31cfa7b37b5b7948b1f06a5110f
olivier <o.gozlan@ajc-ingenierie.fr>
2023-05-23 16:48:40 +0200

    eshop back complet

--- SPRING-BOOT/springBoot/src/test/java/soprasteria/formation/springBoot/CompteTest.java
+++ SPRING-BOOT/springBoot/src/test/java/soprasteria/formation/springBoot/CompteTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import soprasteria.formation.springBoot.entities.Admin;
 import soprasteria.formation.springBoot.entities.Client;
