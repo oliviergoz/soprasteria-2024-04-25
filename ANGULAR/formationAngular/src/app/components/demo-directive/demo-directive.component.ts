06df9f774ba99bbb786fc0fe0d1acbff395a1096
olivier <o.gozlan@ajc-ingenierie.fr>
2023-05-25 15:05:09 +0200

    ngFor

--- ANGULAR/formationAngular/src/app/components/demo-directive/demo-directive.component.ts
+++ ANGULAR/formationAngular/src/app/components/demo-directive/demo-directive.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produit } from 'src/app/models/produit';
 
 @Component({
   selector: 'app-demo-directive',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo-directive.component.css'],
 })
 export class DemoDirectiveComponent {
+  //tabString:Array<string>=[]
+  //tabString: string[] = ['aaa', 'bbb', 'ccc'];
+
+  tabString = ['aaa', 'bbb', 'ccc'];
+  tabProduit: Produit[] = [
+    new Produit('tele', 1000),
+    new Produit('telephone', 500),
+    new Produit('ordinateur', 500),
+  ];
+
+  constructor() {
+    this.tabProduit.push(new Produit('souris', 5));
+  }
+
   display() {
     return false;
   }
--- ANGULAR/formationAngular/src/app/components/personne/personne.component.ts
+++ ANGULAR/formationAngular/src/app/components/personne/personne.component.ts
@@ -8,6 +8,7 @@ import { Personne } from 'src/app/models/personne';
 })
 export class PersonneComponent {
   personneAEnvoyer = new Personne();
+  personnes: Personne[] = [];
 
   recuperationPersonne(personne: Personne) {
     this.personneAEnvoyer = personne;
